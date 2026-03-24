d7294705a431abed88e8c74d73f3988bc2b08130
Амайя <amaya@MacBook-Air-Amaja.local>
2026-03-24 23:41:03 +0600

    first commit

--- cart/models.py
+++ cart/models.py
@@ -8,11 +8,21 @@ class Cart(models.Model):
     def __str__(self):
         return self.user.username
 
+    def total_price(self):
+        total = 0
+        for item in self.items.all():
+            total += item.total_price()
+        return total
+
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.IntegerField(default=1)
+
+    quantity = models.PositiveIntegerField(default=1)
 
     def __str__(self):
         return self.product.name
+
+    def total_price(self):
+        return self.product.price * self.quantity
 
--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='product_images/')
+    image = models.ImageField(upload_to='product/')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
